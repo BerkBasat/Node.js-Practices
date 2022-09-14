df6ff2d5d91d43e5e72730bce0a5eb5494d87686
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-09-14 18:19:29 +0300

    .

--- Store Api/controllers/products.js
+++ Store Api/controllers/products.js
@@ -1,10 +1,28 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req,res) => {
-    throw new Error('Testing async error')
-    res.status(200).json({msg:'Products testing route'})
+    const products = await Product.find({featured:true})
+    res.status(200).json({products, productCount: products.length})
 }
 
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg:'Products route'})
+    const { featured, company, name } = req.query
+    const queryObj = {}
+
+    if(featured) {
+        queryObj.featured = featured === 'true' ? true : false
+    }
+    if(company) {
+        queryObj.company = company
+    }
+    if(name) {
+        queryObj.name = { $regex: name, $options: 'i' }
+    }
+
+    console.log(queryObj)
+
+    const products = await Product.find(queryObj)
+    res.status(200).json({products, productCount: products.length})
 }
 
 module.exports = {
