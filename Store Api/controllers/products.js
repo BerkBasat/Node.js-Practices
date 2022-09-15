b0425bf2a81aac5c5c72ee88f425c48d4bb0f024
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-09-15 22:35:32 +0300

    Project created and set up

--- Store Api/controllers/products.js
+++ Store Api/controllers/products.js
@@ -1,10 +1,5 @@
 const Product = require('../models/product')
 
-const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({featured:true})
-    res.status(200).json({products, productCount: products.length})
-}
-
 const getAllProducts = async (req,res) => {
     const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObj = {}
