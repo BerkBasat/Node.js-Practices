76c6af4632c00f196f33ade73ccf697459a61cb6
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-09-22 10:01:47 +0300

    finished

--- Jobs API/models/User.js
+++ Jobs API/models/User.js
@@ -3,39 +3,46 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, 'Username is required!'],
-        maxlength: 50
-    },
-    email:{
-        type:String,
-        required:[true, 'Email is required!'],
-        maxlength: 50,
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,'Please provide a valid email!'    
-        ],
-        unique: true
-    },
-    password:{
-        type:String,
-        required:[true, 'Password is required!'],
-        minlength: 6
-    },
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    maxlength: 50,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
 })
 
-UserSchema.pre('save', async function(next){
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
-UserSchema.methods.createJWT = function (){
-    return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME })
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  )
 }
 
-UserSchema.methods.comparePassword = async function (userPassword){
-    const isMatch = await bcrypt.compare(userPassword, this.password)
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password)
+  return isMatch
 }
 
 module.exports = mongoose.model('User', UserSchema)
--- Jobs API/routes/jobs.js
+++ Jobs API/routes/jobs.js
@@ -1,15 +1,16 @@
 const express = require('express')
+
 const router = express.Router()
+const {
+  createJob,
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  getJob,
+} = require('../controllers/jobs')
 
+router.route('/').post(createJob).get(getAllJobs)
 
-const { 
-    getAllJobs,
-    getJob,
-    createJob,
-    updateJob,
-    deleteJob } = require('../controllers/jobs')
-
-router.route('/').get(getAllJobs).post(createJob)
 router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
 
 module.exports = router
